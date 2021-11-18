e2028af04f82fbc3a9e7a2609702ac1cd4127a86
spzkk <spzkdejay@gmail.com>
2021-11-18 18:15:25 +0900

    .

--- app/src/main/java/com/example/test/CustomResponse.kt
+++ app/src/main/java/com/example/test/CustomResponse.kt
@@ -1,40 +1,31 @@
 package com.example.test
 
-import android.os.Parcel
-import android.os.Parcelable
-import org.json.JSONObject
 
-
-data class CustomResponse(val cc: String, val aa: Int, val ff: Float = 1.1f){
-//    var a: String? = null
-//    var b: Int = 0
-    var arr: List<Inner>? = null
-//    var jsonObj: List<Inner>? = null
-//    var innerClz: Inner? = null
+data class CustomResponse(val cc: String, val aa: Int, val ff: Float = 1.1f) {
+    var arr: ArrayList<Inner>? = null
 
     override fun toString(): String {
-//        return "a=$a, b=$b, arr:${arr?.joinToString()}, jsonObj:${jsonObj?.joinToString()}, innerClz:($innerClz)"
         val str = arr?.joinToString()
-        return "cc:$cc, aa:$aa, ff:$ff, arr:$str"
+        return "{cc:$cc, aa:$aa, ff:$ff, arr:[$str]}"
     }
 
-    class Inner{
+    class Inner {
         var c: String? = null
         var d: Int = 0
         var innerList: List<Inner2>? = null
 
         override fun toString(): String {
-            return "c:$c, d:$d, innerList:${innerList?.joinToString()}"
+            return "{c:$c, d:$d, innerList:[${innerList?.joinToString()}]}"
         }
     }
 
-    class Inner2{
+    class Inner2 {
         var c: String? = null
         var d: Int = 0
         var innerList: List<String>? = null
 
         override fun toString(): String {
-            return "c:$c, d:$d, innerList:${innerList?.joinToString()}"
+            return "{c:$c, d:$d, innerList:[${innerList?.joinToString()}]}"
         }
     }
 }
--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,35 +1,18 @@
 package com.example.test
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.network.*
 import com.example.network.content.ClassConverter
+import com.example.network.content.handler.ContentHandler
 import com.example.test.databinding.ActivityMainBinding
-import com.example.test.ui.login.LoginActivity
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import kotlinx.coroutines.delay
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.File
 import java.io.InputStream
-import java.net.ContentHandler
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import kotlin.reflect.KParameter
-import kotlin.reflect.full.createInstance
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.typeOf
-
-private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,72 +23,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btnTest1 -> {
-                Api.get<CustomResponse>("http://192.168.1.127:3000").onResult {
-//                    val obj = GsonBuilder().create().fromJson(it, CustomResponse::class.java)
-                    Log.d(TAG, "onClick: obj=$it")
-//                    val json = GsonBuilder().create().toJson(obj)
-//                    Log.d(TAG, "onClick: json=$json")
+                Api.get<CustomResponse>("http://localhost").onResult {
+                    Log.d(TAG, "onResult: $it")
+                    Log.d(TAG, "onResult to json: ${ClassConverter.toJSONObject(it)}")
                 }.onFail { code, error ->
-                    Log.e(TAG, "onClick: $code / $error")
+                    Log.e(TAG, "onFail: $code / $error")
                 }
             }
             R.id.btnTest2 -> {
-                Api.get<DClass>("http://192.168.1.127:3000/list")
-                    .onResult {
-//                        val obj = GsonBuilder().create().fromJson(it, List<TClass>::class.java)
-                        Log.d(TAG, "onClick: obj=$it")
-//                        val json = GsonBuilder().create().toJson(it)
-//                        Log.d(TAG, "onClick: json=$json")
-//                        Log.d(TAG, "onResult: ${it.joinToString()}")
-                    }.onFail{ code, error ->
+                Api.get<List<ElementClass>>("http://localhost/list")
+                    .apply {
+                        //tag = ""
+                        //contentType = "Application/json" //default
+                    }.onResult {
+                        Log.d(TAG, "onResult: $it")
+                    }.onFail { code, error ->
                         Log.e(TAG, "onFail: $code / $error")
                     }
 
-
-//                val param = JSONObject().put("qq", "qq").put("zz", 22)
-//                Api.post<Api.Response>("http://192.168.1.127:3000", param.toString()).onResult {
-//                    Log.d(TAG, "onClick: ${it}, ${Looper.getMainLooper().isCurrentThread}")
-//                    binding.textView.visibility =
-//                        if (binding.textView.visibility == View.VISIBLE) View.GONE else View.VISIBLE
-//                    binding.textView.text = it.getResponse()
-//                    startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-//                }.onFail { code, error ->
-//                    Log.e(TAG, "onClick: ${code} / $error")
-//                }.onPost {
-//                    Log.d(TAG, "onClick:post ${it}, ${Looper.getMainLooper().isCurrentThread}")
-//                    binding.textView.visibility =
-//                        if (binding.textView.visibility == View.VISIBLE) View.GONE else View.VISIBLE
-//                    binding.textView.text = it.getResponse()
-////                    binding.textView.text = it
-//                }
-
-
+                Api.get<CustomResponse>("http://localhost", Request.RequestOption())
+                    .apply {
+                        tag = "b"
+                    }
+                    .onResult {
+                    }
+                    .onFail { code, msg ->
+                    }
             }
             R.id.btnTest3 -> {
-                val param = ClassConverter.toJSON(TClass(22))
-                Log.d(TAG, "ClassConverter.toJSONString(): $param")
+                Api.post<CustomResponse>("http://localhost", Request.RequestOption())
+                    .apply {
+                        tag = "a"
+                    }
+                    .setContentHandler(object : ContentHandler<CustomResponse> {
+                        override fun getContent(inputStream: InputStream): CustomResponse {
+                            TODO("Not yet implemented")
+                        }
+                    })
+                    .onResult {
+                    }
+                    .onFail { code, msg ->
+                    }
             }
             R.id.btnTest4 -> {
-
+                Api.cancel()
             }
         }
     }
 
-    data class DClass(val a: Int, val b: String, val c: String = "c", var aa: String){
-        var ee: String? = null
-    }
-
-    class TClass(val a: Int, val ss:String = "11"){
-        val arr = arrayOf(InnerClass(1), InnerClass((2)))
-
-        override fun toString(): String {
-            return "a:$a, ss:$ss, arr:${arr.joinToString()}"
-        }
-    }
-
-    class InnerClass(val inner1: Int){
-        override fun toString(): String {
-            return "inner1:$inner1"
-        }
-    }
+    data class ElementClass(val value: String)
 }
